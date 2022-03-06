e660d389881b30f0ba0a3e8124af74ff459dfefd
jung <wjddn3711@naver.com>
2022-03-06 22:28:38 +0900

    binary search study

--- tree/practice.py
+++ tree/practice.py
@@ -4,22 +4,6 @@ class Node:
         self.left = None
         self.right = None
 
-def insert(self, value):
-    self.current_node = self.head
-    while True:
-        if value < self.current_node.value:
-            if self.current_node.left != None:
-                self.current_node = self.current_node.left
-            else:
-                self.current_node.left = Node(value)
-                break
-        else:
-            if self.current_node.right != None:
-                self.current_node = self.current_node.right
-            else:
-                self.current_node.right = Node(value)
-                break
-
 
 class NodeMgmt:
     def __init__(self, head):
@@ -51,6 +35,85 @@ class NodeMgmt:
             else:
                 self.current_node = self.current_node.right
         return False
+
+    def delete(self, value):
+        # 삭제할 노드 탐색
+        searched = False
+        self.current_node = self.head
+        self.parent = self.head
+        while self.current_node:
+            if self.current_node.value == value:
+                searched = True
+                break
+            elif value < self.current_node.value:
+                self.parent = self.current_node
+                self.current_node = self.current_node.left
+            else:
+                self.parent = self.current_node
+                self.current_node = self.current_node.right
+
+        if searched == False:
+            return False
+
+            # case1
+        if  self.current_node.left == None and self.current_node.right == None:
+            if value < self.parent.value:
+                self.parent.left = None
+            else:
+                self.parent.right = None
+
+        # case2
+        elif self.current_node.left != None and self.current_node.right == None:
+            if value < self.parent.value:
+                self.parent.left = self.current_node.left
+            else:
+                self.parent.right = self.current_node.left
+        elif self.current_node.left == None and self.current_node.right != None:
+            if value < self.parent.value:
+                self.parent.left = self.current_node.right
+            else:
+                self.parent.right = self.current_node.right
+
+                # case 3
+        elif self.current_node.left != None and self.current_node.right != None:
+            # case3-1
+            if value < self.parent.value:
+                self.change_node = self.current_node.right
+                self.change_node_parent = self.current_node.right
+                while self.change_node.left != None:
+                    self.change_node_parent = self.change_node
+                    self.change_node = self.change_node.left
+                if self.change_node.right != None:
+                    self.change_node_parent.left = self.change_node.right
+                else:
+                    self.change_node_parent.left = None
+                self.parent.left = self.change_node
+                self.change_node.right = self.current_node.right
+                self.change_node.left = self.change_node.left
+            # case 3-2
+            else:
+                self.change_node = self.current_node.right
+                self.change_node_parent = self.current_node.right
+                while self.change_node.left != None:
+                    self.change_node_parent = self.change_node
+                    self.change_node = self.change_node.left
+                if self.change_node.right != None:
+                    self.change_node_parent.left = self.change_node.right
+                else:
+                    self.change_node_parent.left = None
+                self.parent.right = self.change_node
+                self.change_node.right = self.current_node.right
+                self.change_node.left = self.current_node.left
+
+        return True
+
+'''
+- depth (트리의 높이) 를 h라고 표기한다면, O(h)
+  - n개의 노드를 가진다면, $h = log_2{n} $ 에 가까우므로, 시간 복잡도는 $ O(log{n}) $ 
+     - 참고: 빅오 표기법에서 $log{n}$ 에서의 log의 밑은 10이 아니라, 2입니다.
+       - 한번 실행시마다, 50%의 실행할 수도 있는 명령을 제거한다는 의미. 즉 50%의 실행시간을 단축시킬 수 있다는 것을 의미함
+'''
+
 head = Node(1)
 BST = NodeMgmt(head)
 BST.insert(2)
