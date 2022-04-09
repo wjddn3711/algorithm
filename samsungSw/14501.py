cf3d33b55c5331aa8c586f5ccffe40d85c856862
jung <wjddn3711@naver.com>
2022-04-09 20:48:51 +0900

    samsung sw 역량평가 문제풀이 - 브루트포스, dp 문제의 뒤에서 부터 접근을 하여 공식을 만들어 보자면 N번째 날은 N+1번째 날 기준 수익(dp)과 N번째 날 수익 + Tn 만큼 지난 후 수익(dp)중 큰 값을 골라야합니다.

--- samsungSw/14501.py
+++ samsungSw/14501.py
@@ -3,14 +3,25 @@ plan = []
 
 for i in range(n):
     t,p = map(int,input().split())
-    plan.append([p/t, i, t, p])
+    plan.append([t,p])
 
-plan.sort()
-print(plan)
-result = 0
-while plan:
-    curr = plan.pop(0) # 가장 효율이 좋은 일을 꺼내온다
-    if curr[1]+curr[2] > n+1: # n+1 일에는 회사에 없기 때문에 불가능
-        continue
-
+# def solution(n):
+#     dp = [[0] for _ in range(n+1)]
+#     # 점화식 : d[i] = i 번째 날까지 최대 이익을
+#     # i 번째 날은 i+1 번째 날 기준 수익과 i 번째 날 수익 + t[i]만큼 지난후의 수익중 큰값
+#     for i in range(n-1,-1,-1):
+#         if plan[i][0] + i > n: # n+1 일 이후에는 고려할 필요가 없기 때문에 무시
+#             dp[i] = dp[i+1]
+#         else:
+#             dp[i] = max(plan[i][1] + dp[i+plan[i][0]],dp[i+1])
+#     return dp
+dp = [0 for _ in range(n+1)]
+# 점화식 : d[i] = i 번째 날까지 최대 이익을
+# i 번째 날은 i+1 번째 날 기준 수익과 i 번째 날 수익 + t[i]만큼 지난후의 수익중 큰값
+for i in range(n-1,-1,-1):
+    if plan[i][0] + i > n: # n+1 일 이후에는 고려할 필요가 없기 때문에 무시
+        dp[i] = dp[i+1]
+    else:
+        dp[i] = max(plan[i][1] + dp[i+plan[i][0]],dp[i+1])
+print(dp[0])
 
